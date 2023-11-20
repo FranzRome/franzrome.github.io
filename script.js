a430152352034860f8ebb4b4d2755f417f75f9ca
Franz <francesco.romeo-work@libero.it>
2023-11-20 21:10:41 +0100

    Merged Scripts in Unique File

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+// Menu button interaction (Works only using Jquery)
+/*
 $(document).ready(function(){
     // Open menu panel
     $(".menu-button").on('click', function(){
-        console.log("ciao");
         $(".menu-panel").toggleClass("menu-panel--open");
         $(".bar1").toggleClass("bar1--open");
         $(".bar2").toggleClass("bar2--open");
@@ -9,3 +10,42 @@ $(document).ready(function(){
     });
 
 });
+*/
+
+// Prevent images to be dragged around
+let notDraggable = document.querySelectorAll('img,a');
+
+for (let e of notDraggable) {
+   e.setAttribute('draggable', 'false');
+}
+
+// Detect when an object is shown and play the transition
+const observer = new IntersectionObserver((entries) => {
+   entries.forEach((entry) => {
+      //console.log(entry);
+      if (entry.isIntersecting) {
+         entry.target.classList.add('show');
+      }
+   });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+// Modal Functions
+function disableScrolling() {
+   document.documentElement.setAttribute('class', 'no-scroll');
+}
+function enableScrolling() {
+   document.documentElement.setAttribute('class', '');
+}
+
+function openModal() {
+   document.getElementById('modal-avatar').style.display = 'flex';
+   disableScrolling();
+}
+
+function closeModal() {
+   document.getElementById('modal-avatar').style.display = 'none';
+   enableScrolling();
+}
